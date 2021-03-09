410833fe21e059e9e65eae4c9b434168ef366386
vadym <subtobug@gmail.com>
2021-03-09 06:51:13 +0200

    choosing schon eine gute version. Müssen noch die daten richtig bekommen(von DB) und paar Kleinigkeiten überarbeitet

--- client/src/components/Choosing/choosing.jsx
+++ client/src/components/Choosing/choosing.jsx
@@ -6,22 +6,36 @@ import {Col, Container, Row} from "react-bootstrap";
 
 
 let data = new Map();
-data.set("Batman", 0).set("Hobbit", 0).set("Matrix", 0).set("Bond", 0);
+data.set("Batman", 0).set("Hobbit", 0).set("Matrix", 0).set("Bond", 0);// Diese Daten mussen von DB stammen
 
-let filmsArray = Array.from(data, ([name]) => ({name}));
 
-let counterFirstItem = 0;
+let counterFirstItem = 0;// dies auch lokal gespeichert
 
 const Choosing = (props) => {
+    let toTheLobby = props.toTheLobby;
 
+    let filmsArray = Array.from(data, ([name]) => ({name}));// Dies wird aber lokal gespeichert
     let currentFilm = filmsArray[counterFirstItem].name;
     let plusPoint = (props) => {
 
         data.set(currentFilm, data.get(currentFilm) + props);
         renderEntireTree();
-        if(counterFirstItem >= filmsArray.length-1) return
-        counterFirstItem++;
+        if (counterFirstItem >= filmsArray.length - 1) {
+            return toTheLobby(data);
+        }//scoreTogether(); wenn alle filme durch sind,
+        else counterFirstItem++;       //wird diese F gerufen und sortierte Map in die Rating.jsx zurückgegeben
     };
+
+    const scoreTogether = () => {
+        //hier müssen die Scores aller Players addiert werden und als return zurückgegeben werden
+        for (let i = 0; i < filmsArray.length; i++) {//array.length
+            let film = filmsArray[i];
+            // for (){//players.number.length
+            //     data.set(film, data.get(film))//
+            // }
+        }
+
+    }
 
 
     const showScore = () => {
