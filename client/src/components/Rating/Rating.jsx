d0361a08486f45816d29a062933330c794a637f0
vadym <subtobug@gmail.com>
2021-03-09 06:49:07 +0200

    rating seite. Hier werden die Winners dargezeigt

---
+++ client/src/components/Rating/Rating.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Rating = (props) => {
+const show = () => {
+
+        console.log(props.data);
+
+}
+    return(
+        <div>
+            <button onClick={show}>ff</button>
+            tesss
+        </div>
+    )
+};
+
+export default Rating;
