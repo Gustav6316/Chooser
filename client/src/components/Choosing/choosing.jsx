d44823b4a073f16fa20e1ba17ea82f9ddf371ad1
vadym <subtobug@gmail.com>
2021-03-13 03:56:15 +0200

    kann net hinkriegen, werte aus db lokal zu speiccher  und aufrufen. Siehe 11-25 in choosing.jsx

--- client/src/components/Choosing/choosing.jsx
+++ client/src/components/Choosing/choosing.jsx
@@ -1,46 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import Item from "./Item/Item"
 import "bootstrap/dist/css/bootstrap.css";
-import {Col, Container, Row, Button} from "react-bootstrap";
-import api, {getCards} from '../api';
-
+import {Col, Container, Row} from "react-bootstrap";
+import api from '../api';
 
 
 const Choosing = (props) => {
-
-    const test = (sessionid) => {
-        api.get(`/cards/${sessionid}`)
-         .then(res => {
-          let cards = res.data;
-          console.log(cards);
-          return cards
-        });
-    }
-
-    let data2 = getCards('test');//props.room  - für später
-
-    let data = new Map();
-    data.set("Batman", 0).set("Hobbit", 0).set("Matrix", 0).set("Bond", 0);// Diese Daten mussen von DB stammen
-
     const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        // app.post('http://localhost:5000/api/cards')
-        //     .then(response => response.json())
-        //     .then(data1 => setData1(data1));
-    });
+    let filmsArray = [];
+    let endFilms = {};
+    let currentFilm = '';
+    const test = () => {
+        api.get(`/cards/${'test'}`)
+            .then(res => {
+                let cards = res.data;
+
+                for (let i = 0; i < cards.length; i++) {
+                    filmsArray[i] = cards[i].subject;
+                    endFilms[i] = cards[i];
+                }
+            });
+        //currentFilm = endFilms[count].subject;
+
+    }
+
+    test();
 
 
-    let filmsArray = Array.from(data, ([name]) => ({name}));// Dies wird aber lokal gespeichert
-    let currentFilm = filmsArray[count].name;
+
+
     let plusPoint = (props1) => {
 
-        data.set(currentFilm, data.get(currentFilm) + props1);
+        endFilms[count].score += props1;
         if (count >= filmsArray.length - 1) {
 
-            return props.toTheLobby(data);
-        }//scoreTogether(); wenn alle filme durch sind,
-        else setCount(count + 1);       //wird diese F gerufen und sortierte Map in die Rating.jsx zurückgegeben
+            return props.toTheLobby(endFilms);
+        } else setCount(count + 1);       //wird diese F gerufen und sortierte Map in die Rating.jsx zurückgegeben
     };
 
     // const scoreTogether = () => {
@@ -54,16 +50,11 @@ const Choosing = (props) => {
     //
     // }
 
-
-    const showScore = () => {
-        return "Movie " + currentFilm + " has now " + data.get(currentFilm) + " point"
-    }
-
 // String verkürzt von "String" + Value + "String" auf `String ${value} more String`
     return (
         <Container align-items='center'>
 
-            <Button variant='danger' onClick={test('test')}> Test Function </Button>
+
             <Row className="justify-content-md-center">
 
                 <Col md="auto">
@@ -71,10 +62,11 @@ const Choosing = (props) => {
                 </Col>
 
             </Row>
-            <Row><Col>{`Movie Batman now has ${data.get('Batman')} points`}</Col></Row>
-            <Row><Col>{`Movie Hobbit now has ${data.get('Hobbit')} points`}</Col></Row>
-            <Row><Col>{`Movie Matrix now has ${data.get('Matrix')} points`}</Col></Row>
-            <Row><Col>{`Movie Bond now has ${data.get('Bond')} points`}</Col></Row>
+            {/*<Row><Col>{`Movie Batman now has ${endFilms[0].score} points`}</Col></Row>*/}
+            {/*<Row><Col>{`Movie Batman now has ${endFilms[1].score} points`}</Col></Row>*/}
+            {/*<Row><Col>{`Movie Batman now has ${endFilms[2].score} points`}</Col></Row>*/}
+            {/*<Row><Col>{`Movie Batman now has ${endFilms[3].score} points`}</Col></Row>*/}
+
         </Container>
 
     );
